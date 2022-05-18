f92f3f10faeb10519dde81c3ec277f8c23ae7ed9
Milan Gardian <mgardian@gmail.com>
2022-05-17 22:12:44 -0600

    Add build script and workflow

---
+++ Build.ps1
@@ -0,0 +1,22 @@
+﻿#Requires -version 7
+
+# GitHub Actions workflow commands:
+# https://pakstech.com/blog/github-actions-workflow-commands/
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
+
+$ErrorActionPreference = "Stop"
+$homeDir = Get-Location
+
+# copy binaries
+Write-Host "::group::Copy binaries"
+$distDir = (New-Item -Force -ItemType Directory $homeDir/dist).FullName
+Get-ChildItem $env:ProgramW6432/IIS*
+Copy-Item -Recurse "$env:ProgramW6432/IIS Express" $distDir/x64
+Get-ChildItem ${env:ProgramFiles(x86)}/IIS*
+Copy-Item -Recurse "${env:ProgramFiles(x86)}/IIS Express" $distDir/x86
+Write-Host "::endgroup::"
+
+# package binaries
+Write-Host "::group::Package binaries"
+7z a -r -mx $homeDir/iisexpress.7z $distDir/*
+Write-Host "::endgroup::"
