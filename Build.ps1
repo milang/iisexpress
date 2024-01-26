ffe9924d3b25439e21d667474f2bcc0e6326ef26
Milan Gardian <mgardian@gmail.com>
2024-01-26 15:35:00 -0700

    Bump to IIS Express 10.0.25973.1000
    - Update "actions/upload-artifct" to the latest version 4
    - Include links to used GitHub Actions
    - Add VSCode settings for PowerShell and markdownlint
    - Synchronize PowerShell script with my latest style

--- Build.ps1
+++ Build.ps1
@@ -1,22 +1,25 @@
-﻿#Requires -version 7
+﻿#!/usr/bin/env pwsh
+#spell-checker:words
+#Requires -Version 7
 
 # GitHub Actions workflow commands:
 # https://pakstech.com/blog/github-actions-workflow-commands/
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
 
+Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
-$homeDir = Get-Location
 
-# copy binaries
+# Copy binaries.
 Write-Host "::group::Copy binaries"
-$distDir = (New-Item -Force -ItemType Directory $homeDir/dist).FullName
-Get-ChildItem $env:ProgramW6432/IIS*
+$workDir = Get-Location
+$distDir = New-Item -ItemType Directory $workDir/dist
+Get-ChildItem $env:ProgramW6432/IIS* # Keeping this to see what IIS directories are available in the runner in x64 program files directory.
 Copy-Item -Recurse "$env:ProgramW6432/IIS Express" $distDir/x64
-Get-ChildItem ${env:ProgramFiles(x86)}/IIS*
+Get-ChildItem ${env:ProgramFiles(x86)}/IIS* # Keeping this to see what IIS directories are available in the runner in x86 program files directory.
 Copy-Item -Recurse "${env:ProgramFiles(x86)}/IIS Express" $distDir/x86
 Write-Host "::endgroup::"
 
-# package binaries
+# Package binaries.
 Write-Host "::group::Package binaries"
-7z a -r -mx $homeDir/iisexpress.7z $distDir/*
+7z a -r -mx $workDir/iisexpress.7z $distDir/*
 Write-Host "::endgroup::"
